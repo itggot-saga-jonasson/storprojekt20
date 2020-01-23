82545a7c36c0ef241da945ee447504fc11d54198
itggot-saga-jonasson <saga.jonasson@elev.ga.ntig.se>
2020-01-23 10:46:28 +0100

    added login system and encryption of passwords (w/o salt)

--- project/app.rb
+++ project/app.rb
@@ -16,25 +16,20 @@ post("/login") do
     password = params["password"]
     p username
     p password
-    result = db.execute("SELECT user_id FROM users WHERE username=?", username)
+    p BCrypt::Password.create(password)
+    result = db.execute("SELECT userID FROM users WHERE username=?", username)
     p result
     if result.empty?
         error = "user doesn't exist"
         p "it not be working"
     else
-        if db.execute("SELECT password_digest FROM users WHERE user_id=?", result) == password
+        password_digest = db.execute("SELECT password_digest FROM users WHERE userID=?", result)[0][0]
+        if BCrypt::Password.new(password_digest) == password
             error = "login successful!"
         else
-            error = "incorrect assword"
+            error = "incorrect password"
         end
-        # login = db.execute("SELECT user_id FROM users WHERE password_digest=?", password)
-        # if login.empty?
-        #     error = "Password doesn't match username."
-        #     p "it be working"
-        # else
-        #     error = "login successful!"
-        #     p "it be working2"
-        # end
+        
     end
 
     redirect to ("/")
